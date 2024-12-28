2c5af8b5782404a4d606c7b43a6e4ed7481a01c6
MuneebWaliKhan09 <muneebwali809@gmail.com>
2024-12-28 18:55:42 +0500

    c

--- src/animations/ReAnimated6.js
+++ src/animations/ReAnimated6.js
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 import {
   GestureHandlerRootView,
@@ -17,11 +17,11 @@ const ReAnimated6 = () => {
   const animations = useSharedValue(0);
 
   const GestureHandler = Gesture.Pan()
-    .onChange((event) => {
-        animations.value =  event.translationX;
+    .onChange(event => {
+      animations.value = event.translationX;
     })
     .onEnd(() => {
-     animations.value = withTiming(0,{duration:500});
+      animations.value = withTiming(0, {duration: 500});
     });
 
   const animatedStyle = useAnimatedStyle(() => {
@@ -29,6 +29,23 @@ const ReAnimated6 = () => {
       transform: [{translateX: animations.value}],
     };
   });
+
+  const animationStyleLeft = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {scale: animations.value > 70 ? withSpring(1.5) : withSpring(0)},
+      ],
+    };
+  });
+
+  const animationStyleRight = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {scale: animations.value < -70 ? withSpring(1.5) : withSpring(0)},
+      ],
+    };
+  });
+
   return (
     <GestureHandlerRootView
       style={{
@@ -51,9 +68,41 @@ const ReAnimated6 = () => {
                 elevation: 5,
                 backgroundColor: 'white',
                 height: '100%',
+                borderRadius: 10,
+                position: 'absolute',
+                zIndex: 10,
               },
               animatedStyle,
             ]}></Animated.View>
+          <View style={{justifyContent: 'center', height: '100%'}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Animated.View
+                style={[
+                  {width: 30, height: 30, marginLeft: 30},
+                  animationStyleLeft,
+                ]}>
+                <Image
+                  source={require('../images/heart-3510.png')}
+                  style={[{height: '100%', width: '100%'}]}
+                />
+              </Animated.View>
+              <Animated.View
+                style={[
+                  {width: 30, height: 30, marginRight: 30},
+                  animationStyleRight,
+                ]}>
+                <Image
+                  source={require('../images/heart-3510.png')}
+                  style={[{height: '100%', width: '100%'}]}
+                />
+              </Animated.View>
+            </View>
+          </View>
         </Animated.View>
       </GestureDetector>
     </GestureHandlerRootView>
