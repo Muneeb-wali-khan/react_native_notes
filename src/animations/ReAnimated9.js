0a465cc79e6a255da88d5dcb0ec7b3f9a36ec96b
MuneebWaliKhan09 <muneebwali809@gmail.com>
2025-01-02 20:16:43 +0500

    c

--- src/animations/ReAnimated9.js
+++ src/animations/ReAnimated9.js
@@ -1,45 +1,51 @@
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import React, {useState} from 'react';
 import Animated, {
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-// loader
+// button and loader
 
 const ReAnimated9 = () => {
   const animation = useSharedValue(0);
-  const [clicked, setIsClicked] = useState(true);
+  const [clicked, setIsClicked] = useState(false);
 
   const animatedStyles = useAnimatedStyle(() => {
+    const width = interpolate(animation.value, [0, 1], [230, 55]);
+    const height = interpolate(animation.value, [0, 1], [55, 55]);
+    const borderRadius = interpolate(animation.value, [0, 1], [50, 100]);
     return {
-      width: animation.value > 0 ? 0 : 230,
-      height: animation.value > 0 ? 0 : 55,
+      width,
+      height,
+      borderRadius,
     };
   });
 
   return (
     <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Animated.View>
+      <Animated.View
+        style={[
+          {
+            borderRadius: 50,
+            borderWidth: 1,
+            overflow: 'hidden',
+          },
+          animatedStyles,
+        ]}>
         <TouchableOpacity
           onPress={() => {
-            if (clicked) {
-              animation.value = withTiming(1, {duration: 500});
-            } else {
-              animation.value = withTiming(0, {duration: 500});
-            }
+            animation.value = withTiming(clicked ? 0 : 1, {duration: 500});
             setIsClicked(!clicked);
           }}
           style={[
             {
-                width:230,
-                height:55,
-              borderWidth: 1,
-              borderRadius: 50,
               justifyContent: 'center',
               backgroundColor: 'purple',
+              width: '100%',
+              height: '100%',
             },
-            animatedStyles,
           ]}>
           {clicked ? (
             <Image
@@ -48,12 +54,14 @@ const ReAnimated9 = () => {
             />
           ) : (
             <Text
-              style={{
-                textAlign: 'center',
-                fontSize: 16,
-                fontWeight: '700',
-                color: 'white',
-              }}>
+              style={[
+                {
+                  textAlign: 'center',
+                  fontSize: 16,
+                  fontWeight: '700',
+                  color: 'white',
+                },
+              ]}>
               Login
             </Text>
           )}
